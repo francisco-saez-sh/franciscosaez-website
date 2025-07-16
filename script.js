02840ce91b6ea76f0783d4c62d621fd797be3b2b
francisco-saez-sh <franciscosaez.sh@gmail.com>
2025-07-16 19:18:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Navbar
+const menu = document.querySelector('.menu')
+const navbar = document.querySelector('.navbar')
+const navlist = document.querySelector('.nav-list')
+ 
+menu.addEventListener('click', () => {
+  navbar.classList.toggle('change')
+  menu.classList.toggle('change')
+})
+ 
+navlist.addEventListener('click', () => {
+    navbar.classList.toggle('change')
+    menu.classList.toggle('change')
+  }
+);
+// End of Navbar
